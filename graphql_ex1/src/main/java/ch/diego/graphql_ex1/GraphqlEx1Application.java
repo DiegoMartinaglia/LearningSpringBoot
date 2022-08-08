cf4596ea59a3f418b4e77c4302c91c26f9a66a4f
dmartinaglia <dm@netlive.ch>
2022-08-08 17:44:41 +0200

    init graphql_ex1

---
+++ graphql_ex1/src/main/java/ch/diego/graphql_ex1/GraphqlEx1Application.java
@@ -0,0 +1,13 @@
+package ch.diego.graphql_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlEx1Application.class, args);
+	}
+
+}
---
+++ graphql_ex1/src/test/java/ch/diego/graphql_ex1/GraphqlEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.diego.graphql_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
